2bc7d178eab9a75c77a5eea5ddbb3f4a06abf57a
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-20 16:20:20 -0300

    add HerancaTest to extends all id's class

---
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancao/HerancaTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.ecommerce.mapeamentoavancao;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.SexoCliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class HerancaTest extends EntityManagerTest {
+
+    @Test
+    public void salvarItem() {
+        Cliente cliente = new Cliente();
+
+        cliente.setNome("Clariça Finotti");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(clienteVerificacao.getId());
+    }
+
+}
