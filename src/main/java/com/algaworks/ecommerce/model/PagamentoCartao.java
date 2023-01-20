7b266bc98fcebd743b4f3aa591a600b8ce0bdb97
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-20 16:46:26 -0300

    add abstract class Pagamento to PagamentoBoleto and PagamentoCartao

--- src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
+++ src/main/java/com/algaworks/ecommerce/model/PagamentoCartao.java
@@ -10,14 +10,7 @@ import javax.persistence.*;
 @Setter
 @Entity
 @Table(name = "pagamento_cartao")
-public class PagamentoCartao extends EntidadeBaseInteger{
-    @MapsId
-    @OneToOne(optional = false)
-    @JoinColumn(name = "pedido_id")
-    private Pedido pedido;
-
-    @Enumerated(EnumType.STRING)
-    private StatusPagamento status;
-
-    private String numero;
+public class PagamentoCartao extends Pagamento{
+    @Column(name = "numero_cartao")
+    private String numeroCartao;
 }
--- src/test/java/com/algaworks/ecommerce/mapeamentoavancao/HerancaTest.java
+++ src/test/java/com/algaworks/ecommerce/mapeamentoavancao/HerancaTest.java
@@ -1,12 +1,12 @@
 package com.algaworks.ecommerce.mapeamentoavancao;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.Cliente;
-import com.algaworks.ecommerce.model.SexoCliente;
+import com.algaworks.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class HerancaTest extends EntityManagerTest {
 
@@ -24,6 +24,33 @@ public class HerancaTest extends EntityManagerTest {
 
         Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
         Assert.assertNotNull(clienteVerificacao.getId());
+    }
+
+    @Test
+    public void buscarPagamentos(){
+        List<Pagamento> pagamentos = entityManager.createQuery("select p from Pagamento p").getResultList();
+
+        Assert.assertFalse(pagamentos.isEmpty());
+    }
+
+    @Test
+    public void incluirPagamento(){
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        PagamentoCartao pagamentoCartao = new PagamentoCartao();
+        pagamentoCartao.setPedido(pedido);
+        pagamentoCartao.setStatus(StatusPagamento.RECEBIDO);
+        pagamentoCartao.setNumeroCartao("123");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pagamentoCartao);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getPagamento());
+
     }
 
 }
