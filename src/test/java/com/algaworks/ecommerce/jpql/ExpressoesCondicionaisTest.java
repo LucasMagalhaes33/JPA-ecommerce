b82cc6f423598013a11318d1c62a1d8ab6f5899c
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-24 11:29:47 -0300

    test jpql expression return project DTO result

--- src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.dto.ProdutoDTO;
 import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Produto;
 import org.junit.Assert;
@@ -76,6 +77,18 @@ public class ExpressoesCondicionaisTest extends EntityManagerTest {
 
         Assert.assertTrue(lista.get(0).length ==2);
         lista.forEach(arr -> System.out.println(arr[0] + "," + arr[1]));
+    }
+
+    @Test
+    public void projetarNoDTO(){
+        String jpql = "select new com.algaworks.ecommerce.dto.ProdutoDTO(id, nome) from Produto";
+
+        TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(jpql, ProdutoDTO.class);
+        List<ProdutoDTO> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+        lista.forEach(produtoDTO -> System.out.println(produtoDTO.getId() + ", " + produtoDTO.getNome()));
+
     }
 
 }
