69bc82b073255a8d6093c725b7cfe5a29e0447a3
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-24 13:00:06 -0300

    test jpql expression left join on result

--- src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -13,12 +13,24 @@ public class JoinTest extends EntityManagerTest {
     @Test
     public void fazerJoin() {
 
-        String jpql = "select p from Pedido p join p.itens join p.pagamento pag where pag.status = 'PROCESSANDO'";
+        String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
         List<Object[]> lista = typedQuery.getResultList();
 
-        Assert.assertTrue(lista.size() == 3);
+        Assert.assertFalse(lista.isEmpty());
+
+    }
+
+    @Test
+    public void fazerLeftJoin() {
+
+        String jpql = "select p from Pedido p join p.itens join p.pagamento pag";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
 
     }
 
