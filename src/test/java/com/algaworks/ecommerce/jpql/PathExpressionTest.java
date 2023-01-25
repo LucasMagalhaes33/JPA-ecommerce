7d08f3241e94dca37ab0cb77dde5a4686ea6b611
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-25 15:02:12 -0300

    test parameters on jpql queries

--- src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -3,6 +3,7 @@ package com.algaworks.ecommerce.jpql;
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.dto.ProdutoDTO;
 import com.algaworks.ecommerce.model.Cliente;
+import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -10,6 +11,18 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PathExpressionTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPedidosComProdutosEspecificos(){
+        String jpql = "select p from Pedido p " +
+                " join p.itens ip " +
+                " join Produto where ip.produto.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+    }
 
     @Test
     public void usarPathExpressions() {
