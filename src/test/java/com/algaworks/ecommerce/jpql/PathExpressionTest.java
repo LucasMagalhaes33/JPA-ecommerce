c6163bad9231035dd7edbe944d8ace7dcf3ce224
Lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-25 13:44:25 -0300

    test path expressions on queries

---
+++ src/test/java/com/algaworks/ecommerce/jpql/PathExpressionTest.java
@@ -0,0 +1,25 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.dto.ProdutoDTO;
+import com.algaworks.ecommerce.model.Cliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class PathExpressionTest extends EntityManagerTest {
+
+    @Test
+    public void usarPathExpressions() {
+
+        String jpql = "select p.cliente.nome from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+}
