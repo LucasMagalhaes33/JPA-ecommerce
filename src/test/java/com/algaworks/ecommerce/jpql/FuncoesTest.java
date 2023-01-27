581d173b4ca296d474d133e1954af759fdd054d8
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-27 15:39:58 -0300

    test JPA group by queries

--- src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/FuncoesTest.java
@@ -1,19 +1,39 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Categoria;
 import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
 public class FuncoesTest extends EntityManagerTest {
 
     @Test
+    public void aplicarFuncaoAgregacao() {
+        // avg, count, min, max, sum
+
+        String jpql = "select sum(p.total) from Pedido p";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
     public void aplicarFuncaoNativas() {
-        String jpql = "select function('dayname', p.dataCriacao) from Pedido p where function('acima_media_faturamento', p.total) = 1";
+        String jpql = "select function('dayname', p.dataCriacao) from Pedido p " +
+                " where function('acima_media_faturamento', p.total) = 1";
 
         TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
 
@@ -37,8 +57,8 @@ public class FuncoesTest extends EntityManagerTest {
 
     @Test
     public void aplicarFuncaoNumero() {
-        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p" +
-                " where abs(p.total) > 100";
+        String jpql = "select abs(p.total), mod(p.id, 2), sqrt(p.total) from Pedido p " +
+                " where abs(p.total) > 1000";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
@@ -50,11 +70,12 @@ public class FuncoesTest extends EntityManagerTest {
 
     @Test
     public void aplicarFuncaoData() {
-        //TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        //current_date, current_time, current_timestamp
-        //year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
+        // TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        // current_date, current_time, current_timestamp
+        // year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
 
-        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) from Pedido p where hour(p.dataCriacao) > 18";
+        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) " +
+                " from Pedido p where hour(p.dataCriacao) > 18";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
@@ -66,9 +87,10 @@ public class FuncoesTest extends EntityManagerTest {
 
     @Test
     public void aplicarFuncaoString() {
-        //concat, length, locate, substring, lower, upper, trim
+        // concat, length, locate, substring, lower, upper, trim
 
-        String jpql = "select c.nome, length(c.nome) from Categoria c where substring(c.nome, 1, 1) = 'L'";
+        String jpql = "select c.nome, length(c.nome) from Categoria c " +
+                " where substring(c.nome, 1, 1) = 'N'";
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 
@@ -77,5 +99,4 @@ public class FuncoesTest extends EntityManagerTest {
 
         lista.forEach(arr -> System.out.println(arr[0] + " - " + arr[1]));
     }
-
 }
--- src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/PaginacaoJPQLTest.java
@@ -1,26 +1,24 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.dto.ProdutoDTO;
 import com.algaworks.ecommerce.model.Categoria;
-import com.algaworks.ecommerce.model.Cliente;
 import com.algaworks.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PaginacaoJPQLTest extends EntityManagerTest {
 
     @Test
-    public void ordenarResultados() {
+    public void paginarResultados() {
         String jpql = "select c from Categoria c order by c.nome";
 
-        //FIRST_RESULTS = MAX_RESULTS * (pagina - 1)
         TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
-        typedQuery.setFirstResult(2);
+
+        // FIRST_RESULT = MAX_RESULTS * (pagina - 1)
+        typedQuery.setFirstResult(6);
         typedQuery.setMaxResults(2);
 
         List<Categoria> lista = typedQuery.getResultList();
@@ -28,5 +26,4 @@ public class PaginacaoJPQLTest extends EntityManagerTest {
 
         lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
     }
-
 }
