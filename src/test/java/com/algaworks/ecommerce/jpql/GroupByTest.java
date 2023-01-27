2fa92f47b65b9173f9a6e0129fef55a117baa0a1
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-01-27 20:35:28 -0300

    test JPA where with group by

--- src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/algaworks/ecommerce/jpql/GroupByTest.java
@@ -1,7 +1,6 @@
 package com.algaworks.ecommerce.jpql;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.StatusPedido;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +8,35 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class GroupByTest extends EntityManagerTest {
+
+    @Test
+    public void agruparEFiltrarResultado() {
+//         Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+
+//         Total de vendas por categoria.
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c join ip.pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+               " join ip.pedido p join p.cliente c join ip.pedido p " +
+                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) " +
+                 " group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
 
     @Test
     public void agruparResultado() {
@@ -21,9 +49,22 @@ public class GroupByTest extends EntityManagerTest {
 //                " group by year(p.dataCriacao), month(p.dataCriacao) ";
 
 //         Total de vendas por categoria.
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip "
-                +" join ip.produto pro join pro.categorias c "
-                +" group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+//        Total de vendas por dia e por categoria
+        String jpql = "select " +
+                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
+                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
+                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+                " order by p.dataCriacao, c.nome ";
 
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
